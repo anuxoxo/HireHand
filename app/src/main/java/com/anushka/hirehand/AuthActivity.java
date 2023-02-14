67685a3a053e70b0d92cda9c16071ab2e2dec7e5
Kumari Anushka <anushka.ak14@gmail.com>
2023-02-14 23:36:31 +0530

    auth design added

--- app/src/main/java/com/anushka/hirehand/AuthActivity.java
+++ app/src/main/java/com/anushka/hirehand/AuthActivity.java
@@ -3,12 +3,39 @@ package com.anushka.hirehand;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class AuthActivity extends AppCompatActivity {
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    Button googleBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+//        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestEmail().build();
+//        gsc = GoogleSignIn.getClient(this, gso);
+//
+////        googleBtn
+//        googleBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                signIn();
+//            }
+//        });
     }
 }
