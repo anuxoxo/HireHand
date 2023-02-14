55486bc693d45c953c6b30333461e8aca2452688
Kumari Anushka <anushka.ak14@gmail.com>
2023-02-15 00:04:16 +0530

    google auth func added

--- app/src/main/java/com/anushka/hirehand/AuthActivity.java
+++ app/src/main/java/com/anushka/hirehand/AuthActivity.java
@@ -1,15 +1,21 @@
 package com.anushka.hirehand;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.content.Intent;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class AuthActivity extends AppCompatActivity {
 
@@ -26,16 +32,48 @@ public class AuthActivity extends AppCompatActivity {
             getSupportActionBar().hide();
         }
 
-//        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//                .requestEmail().build();
-//        gsc = GoogleSignIn.getClient(this, gso);
-//
-////        googleBtn
-//        googleBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-////                signIn();
-//            }
-//        });
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        googleBtn = findViewById(R.id.googleBtn);
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+
+    void signIn(){
+        Intent signIntent = gsc.getSignInIntent();
+        startActivityForResult(signIntent,1000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1000){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            navigateToUser(account);
+        } catch (ApiException e) {
+            Log.w("Auth Error", "signInResult:failed code=" + e.getStatusCode());
+            Toast.makeText(this, "Something went wrong...", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void navigateToUser(GoogleSignInAccount account){
+        finish();
+        Intent intent = new Intent(AuthActivity.this, UserActivity.class);
+        intent.putExtra("account", account);
+        startActivity(intent);
     }
 }
