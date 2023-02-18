4068d9ea20ad75294a23a6827240865ecdfac7ee
Varsha <91422487+Varsha-Pradeep@users.noreply.github.com>
2023-02-18 15:27:03 +0530

    test 2

--- app/src/main/java/com/anushka/hirehand/UserActivity.java
+++ app/src/main/java/com/anushka/hirehand/UserActivity.java
@@ -10,6 +10,6 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
-        //varsha
+        //varsha branch
     }
 }
